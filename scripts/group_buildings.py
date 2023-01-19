e4b0bb87e9f145562e78befcfa685be667009c65
Likey00 <ishaan.a.ivaturi@gmail.com>
2023-01-19 17:46:11 -0500

    Fixed empty days

--- scripts/group_buildings.py
+++ scripts/group_buildings.py
@@ -2,21 +2,21 @@ import json
 from collections import defaultdict
 from flatten_dict import unflatten
 
-times = json.load(open('times.json'))
+times = json.load(open('../times.json'))
 by_building = defaultdict(set)
 
 for course in times.values():
     for time in course:
-        if time['campusName'] == 'Online':
+        if time['campusName'] == 'Online' or time['buildingCode'] == '':
             continue
         by_building[(
-            time['meetingDay'],
             time['campusName'],
             time['buildingCode'],
             time['roomNumber'],
+            time['meetingDay']
         )].add((int(time['startMinute']), int(time['endMinute'])))
 
 by_building = {k:sorted(list(by_building[k])) for k in sorted(by_building)}
 by_building = unflatten(by_building)
 
-json.dump(by_building, open('rooms.json', 'w'))
+json.dump(by_building, open('rooms.js', 'w'))
